1930596a752637434e46ea9a7a919c9685acb499
erosMariano <erosmariano1@gmail.com>
2022-06-25 12:36:39 -0300

    Atualização

--- assets/js/lp.js
+++ assets/js/lp.js
@@ -1,3 +1,29 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active")
+        if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+
+        }
+
+        accordionItemHeader.classList.toggle("active")
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    })
+})
+
+
+
 const [nome, email, phone] = document.querySelectorAll(".form input")
 const submit = document.querySelector(".form .enviar")
 const errorMessage = document.querySelector(".error")
