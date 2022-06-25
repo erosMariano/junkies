784bdf6af9242c7d219d25714ab37a2f3cab8648
erosMariano <erosmariano1@gmail.com>
2022-06-25 01:28:56 -0300

    att

--- assets/js/lp.js
+++ assets/js/lp.js
@@ -1,29 +1,55 @@
-try {
-    const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+const [nome, email, phone] = document.querySelectorAll(".form input")
+const submit = document.querySelector(".form .enviar")
+const errorMessage = document.querySelector(".error")
 
 
-    accordionItemHeaders.forEach(accordionItemHeader => {
-        accordionItemHeader.addEventListener("click", event => {
-
-            const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active")
-            if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
-                currentlyActiveAccordionItemHeader.classList.toggle("active");
-                currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-
-            }
-
-            accordionItemHeader.classList.toggle("active")
-            const accordionItemBody = accordionItemHeader.nextElementSibling;
-
-            if (accordionItemHeader.classList.contains("active")) {
-                accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
-            } else {
-                accordionItemBody.style.maxHeight = 0;
-            }
-        })
-    })
-
-
-} catch (e) {
-    console.log(e)
+function ValidateEmail(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value.match(mailformat)) {
+        // alert("Valid email address!");
+        return true;
+    } else {
+        // alert("You have entered an invalid email address!");
+        throw new Error("invalid email")
+    }
 }
+
+
+function validateName(inputName) {
+    if (inputName.value !== "" && inputName.value.length > 2) {
+        return true;
+    } else {
+        throw new Error("invalid name")
+    }
+}
+
+function validatePhoneNumber(input_str) {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    return re.test(input_str);
+}
+
+function validateForm(inputPhone) {
+    var phone = inputPhone.value;
+    if (!validatePhoneNumber(phone)) {
+        throw new Error("Please enter a valid phone number")
+    } else {
+        return true
+    }
+}
+
+
+submit.addEventListener("click", () => {
+    try {
+        validateName(nome)
+        ValidateEmail(email);
+        validateForm(phone)
+
+        errorMessage.classList.remove("active")
+
+    } catch (e) {
+        console.log(e.message)
+        errorMessage.classList.add("active")
+        errorMessage.innerHTML = `${e.message}`
+    }
+})
