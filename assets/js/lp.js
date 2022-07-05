8769907e8acc9cfd3d6499b8729b9589089357f6
erosMariano <erosmariano1@gmail.com>
2022-07-04 22:39:41 -0300

    atualização

--- assets/js/lp.js
+++ assets/js/lp.js
@@ -16,7 +16,7 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 
         if (accordionItemHeader.classList.contains("active")) {
             if (accordionItemBody.scrollHeight >= 768) {
-                accordionItemBody.style.maxHeight = "1032px"
+                accordionItemBody.style.maxHeight = "1186px"
             } else {
                 accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
             }
