013a89fb0c73bda2792e7da34d9ecbb94ef95893
erosMariano <erosmariano1@gmail.com>
2022-07-22 11:49:54 -0300

    atualização

--- assets/js/lp.js
+++ assets/js/lp.js
@@ -65,9 +65,9 @@ function validateName(inputName) {
 }
 
 function validatePhoneNumber(input_str) {
-    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
 
-    return re.test(input_str);
+    const valid = !isNaN(parseFloat(input_str)) && isFinite(input_str);
+    return valid
 }
 
 function validateForm(inputPhone) {
